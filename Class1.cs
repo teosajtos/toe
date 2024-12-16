3b94a0076b06ee9e68f43faca1ea2d92f5d6a030
teosajtos <teosajtos937@gmail.com>
2024-12-16 17:47:49 +0100

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+
+using Teo2;
+
+namespace Teo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik person = new Ucenik("Teo", 17);
+            person.Pozdrav();
+        }
+    }
+}
